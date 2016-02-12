0da1cdb493568192dbe81bce76ab4259ad37ba98
Erik Hvatum <ice.rikh@gmail.com>
2016-02-11 21:35:20 -0600

    Add copy calibrations switch.

--- pull_files.py
+++ pull_files.py
@@ -26,12 +26,14 @@ import argparse
 import enum
 from pathlib import Path
 
+SOURCE_5X_DPATH = Path('/mnt/scopearray/Sinha_Drew/20160209_N2Acquisition_1_5x/')
+
 class ZStackAction(enum.Enum):
     CopyZStacks = 0
     MoveZStacks = 1
     IgnoreZStacks = 2
 
-def pull_files(zstack_action, include_newest_zstacks, copy_metadata, copy_other_data):
+def pull_files(zstack_action, include_newest_zstacks, copy_metadata, copy_calibrations, copy_other_data):
     pass
 
 if __name__ == '__main__':
@@ -43,14 +45,16 @@ if __name__ == '__main__':
     parser.add_argument('--copy-zstacks', action='store_true')
     parser.add_argument('--move-zstacks', action='store_true')
     parser.add_argument('--copy-metadata', action='store_true')
+    parser.add_argument('--copy-calibrations', action='store_true')
     parser.add_argument('--copy-other-data', action='store_true')
     args = parser.parse_args()
     if args.copy_zstacks and args.move_zstacks:
         print('The --copy-zstacks and --move-zstacks switches are mutually exclusive.\n', file=sys.stderr)
         parser.print_usage(file=sys.stderr)
         sys.exit(-1)
-    if not any((args.copy_zstacks, args.move_zstacks, args.copy_metadata, args.copy_other_data)):
-        print('At least one of --copy-zstacks, --move-zstacks, --copy-metadata, --copy-other-data must be specified.\n', file=sys.stderr)
+    if not any((args.copy_zstacks, args.move_zstacks, args.copy_metadata, args.copy_calibrations, args.copy_other_data)):
+        print('At least one of --copy-zstacks, --move-zstacks, --copy-metadata, --copy-calibrations, and/or '
+              '--copy-other-data must be specified.\n', file=sys.stderr)
         parser.print_usage(file=sys.stderr)
         sys.exit(-1)
     if args.copy_zstacks:
@@ -59,4 +63,4 @@ if __name__ == '__main__':
         zstack_action = ZStackAction.MoveZStacks
     else:
         zstack_action = ZStackAction.IgnoreZStacks
-    pull_files(zstack_action, args.include_newest_zstacks, args.copy_metadata, args.copy_other_data)
+    pull_files(zstack_action, args.include_newest_zstacks, args.copy_metadata, args.copy_calibrations, args.copy_other_data)
